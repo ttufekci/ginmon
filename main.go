f5a042006531475a542bb8b31e678f03b72e8ed0
talattufekci <talattufekci@gmail.com>
2017-09-04 11:41:29 +0300

    remove unnecessary code

--- main.go
+++ main.go
@@ -10,25 +10,6 @@ import (
 	"github.com/go-fsnotify/fsnotify"
 )
 
-func printError(err error) {
-	if err != nil {
-		os.Stderr.WriteString(fmt.Sprintf("==> Error: %s\n", err.Error()))
-	}
-}
-
-func printOutput(outs []byte) {
-	if len(outs) > 0 {
-		fmt.Printf("==> Output: %s\n", string(outs))
-	}
-}
-
-func kill(cmd *exec.Cmd) error {
-	kill := exec.Command("TASKKILL", "/T", "/F", "/PID", strconv.Itoa(cmd.Process.Pid))
-	kill.Stderr = os.Stderr
-	kill.Stdout = os.Stdout
-	return kill.Run()
-}
-
 // main
 func main() {
 
@@ -52,7 +33,7 @@ func main() {
 	err = fc.Run()
 	if err != nil {
 		fmt.Println("error occurred")
-		goto errorLabel
+		goto buildError
 	}
 
 	fc = exec.Command("test.exe")
@@ -67,7 +48,7 @@ func main() {
 
 	fc.Start()
 
-errorLabel:
+buildError:
 
 	done := make(chan bool)
 
@@ -94,7 +75,6 @@ errorLabel:
 
 				if err != nil {
 					fmt.Println("error occurred when killing process")
-					// goto errorInsideLabel
 				}
 
 				time.Sleep(time.Second * 1)
@@ -102,8 +82,6 @@ errorLabel:
 				fc = exec.Command("cmd", "/C", "go", "build", "testexample/test.go")
 
 				fc.Dir = "C:/gowork/src/github.com/ttufekci/ginmon"
-
-				fmt.Println("testing")
 
 				fc.Stdin = os.Stdin
 
@@ -114,15 +92,12 @@ errorLabel:
 				err = fc.Run()
 
 				if err != nil {
-					fmt.Println("error occurred 2")
-					goto errorInsideLabel
+					goto buildErrorInsideLabel
 				}
 
 				fc = exec.Command("test.exe")
 
 				fc.Dir = "C:/gowork/src/github.com/ttufekci/ginmon"
-
-				fmt.Println("testing")
 
 				fc.Stdin = os.Stdin
 
@@ -132,17 +107,11 @@ errorLabel:
 
 				fc.Start()
 
-			errorInsideLabel:
-
-				fmt.Println("deneme3")
+			buildErrorInsideLabel:
 
 				time.Sleep(time.Second * 1)
 
-				fmt.Println("before restart true")
-
 				restart <- true
-
-				fmt.Println("restart is starting")
 
 			case err := <-watcher.Errors:
 				fmt.Println("ERROR", err)
@@ -154,7 +123,6 @@ errorLabel:
 		for {
 			select {
 			case restarted := <-restart:
-				fmt.Println("restarted another func", restarted)
 				if restarted {
 					if err := watcher.Add("testexample"); err != nil {
 						fmt.Println("ERROR", err)
@@ -166,7 +134,7 @@ errorLabel:
 
 	// out of the box fsnotify can watch a single file, or a single directory
 	if err := watcher.Add("testexample"); err != nil {
-		fmt.Println("ERROR_2", err)
+		fmt.Println("error for watcher: ", err)
 	}
 
 	<-done
--- testexample/test.go
+++ testexample/test.go
@@ -6,8 +6,8 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong9992",
+			"message": "pong9993",
 		})
 	})
-	// r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
